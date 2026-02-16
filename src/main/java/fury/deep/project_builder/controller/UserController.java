1d98e307aa18f22f6a71bc472766698f31051212
Kasodariya Deep <deepkasodariya44@gmail.com>
2026-02-16 18:04:00 +0530

    Removed webmc cause not working with 4

--- src/main/java/fury/deep/project_builder/controller/UserController.java
+++ src/main/java/fury/deep/project_builder/controller/UserController.java
@@ -3,10 +3,6 @@ package fury.deep.project_builder.controller;
 import fury.deep.project_builder.dto.user.UserLoginRequest;
 import fury.deep.project_builder.dto.user.UserRegisterRequest;
 import fury.deep.project_builder.service.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Authentication", description = "User authentication APIs")
 @RestController
 @RequestMapping("/api/v1/auth")
 public class UserController {
@@ -26,28 +21,12 @@ public class UserController {
         this.userService = userService;
     }
 
-    @Operation(
-            summary = "Register new user",
-            description = "Creates a new user account"
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "201", description = "User successfully registered"),
-            @ApiResponse(responseCode = "400", description = "Validation error")
-    })
     @PostMapping("/register")
     public ResponseEntity<Void> registerUser(@Valid @RequestBody UserRegisterRequest userRegisterRequest) {
         userService.registerUser(userRegisterRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @Operation(
-            summary = "Login user",
-            description = "Authenticates a user using credentials"
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Login successful"),
-            @ApiResponse(responseCode = "401", description = "Invalid credentials")
-    })
     @PostMapping("/login")
     public ResponseEntity<Void> loginUser(@Valid @RequestBody UserLoginRequest userLoginRequest) {
         userService.loginUser(userLoginRequest);
