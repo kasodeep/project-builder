bf3e5cbc83a2193d353b9a44382b3c900638b14c
Kasodariya Deep <deepkasodariya44@gmail.com>
2026-02-24 10:56:42 +0530

    Lazy sometimes works better than, eager

--- src/main/java/fury/deep/project_builder/service/task/TaskService.java
+++ src/main/java/fury/deep/project_builder/service/task/TaskService.java
@@ -25,6 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -86,6 +87,7 @@ public class TaskService {
     }
 
     @Autowired
+    @Lazy
     public void setSelf(TaskService self) {
         this.self = self;
     }
